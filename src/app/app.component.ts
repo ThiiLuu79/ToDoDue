99ea5df999c0829df6ccd7be1bf60b0913c6bcaa
The Minh Luong <81328535+ThiiLuu79@users.noreply.github.com>
2025-04-08 14:58:57 -0400

    Cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { TaskCardComponent } from './components/task-card/task-card.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @Component({
