be552206f9981ad22f2bef61f92e216e0a5e100f
The Minh Luong <81328535+ThiiLuu79@users.noreply.github.com>
2025-04-18 17:38:58 -0400

    Added NgRx store

---
+++ src/app/state/calendar/calendar.selector.ts
@@ -0,0 +1,9 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { CalendarState } from './calendar.models';
+
+export const selectCalendarState = createFeatureSelector<CalendarState>('calendar');
+
+export const selectCurrentMonth = createSelector(
+  selectCalendarState,
+  (state: CalendarState) => state.currentMonth
+);
