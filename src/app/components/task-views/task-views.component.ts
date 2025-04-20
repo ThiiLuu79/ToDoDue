b5ed69f35ae1c5e4016c5bff75d2a7b42e9f81c4
The Minh Luong <81328535+ThiiLuu79@users.noreply.github.com>
2025-04-20 13:38:06 -0400

    Minor changes

--- src/app/components/task-views/task-views.component.ts
+++ src/app/components/task-views/task-views.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { TasksLeaderboardComponent } from '../tasks-leaderboard/tasks-leaderboard.component';
 import { CalendarViewComponent } from '../calendar-view/calendar-view.component';
 import { Task } from '../../models/task.model';
@@ -12,12 +12,16 @@ import { CommonModule } from '@angular/common';
   styleUrl: './task-views.component.sass',
   imports: [TasksLeaderboardComponent, CalendarViewComponent, CommonModule],
 })
-export class TaskViewsComponent {
+export class TaskViewsComponent implements OnInit {
   @Input() todoTasks: Task[] = [];
   @Input() inProgressTasks: Task[] = [];
   @Input() doneTasks: Task[] = [];
 
   currentView: string = TododueViews.PRIORITIES;
+
+  ngOnInit(): void {
+    this.loadCurrentViewFromLocalStorage();
+  }
 
   getAllTasks(): Task[] {
     return [...this.todoTasks, ...this.inProgressTasks, ...this.doneTasks];
@@ -25,9 +29,29 @@ export class TaskViewsComponent {
 
   showPriorities(): void {
     this.currentView = TododueViews.PRIORITIES;
+    this.saveCurrentViewToLocalStorage();
   }
 
   showCalendar(): void{
     this.currentView = TododueViews.CALENDAR;
+    this.saveCurrentViewToLocalStorage();
   }
+
+  saveCurrentViewToLocalStorage(): void {
+    if (typeof window !== 'undefined' && window.localStorage) {
+      const currentView = this.currentView;
+      localStorage.setItem('currentView', JSON.stringify(currentView));
+    }
+  }
+  
+  loadCurrentViewFromLocalStorage(): void {
+    if (typeof window !== 'undefined' && window.localStorage) {
+      const storedView = localStorage.getItem('currentView');
+      if (storedView) {
+        const parsedView = JSON.parse(storedView);
+        this.currentView = parsedView;
+      }
+    }
+  }
+
 }
